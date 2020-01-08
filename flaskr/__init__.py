f8fdb98e0fe1cff5ee006fa3e97d95c8c263b139
ahnyoungseon99 <dudtjs0920@naver.com>
2020-01-08 15:20:34 +0900

    the blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,7 +29,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import auth
-    app.register_blueprint(auth.bp)
-
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
     return app
